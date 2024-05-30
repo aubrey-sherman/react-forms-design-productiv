584d1618f55d4414cee53c548c2ee7d5a542b699
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-05-30 18:42:24 -0400

    TodoForm component WIP.

--- productiv/src/TodoForm.jsx
+++ productiv/src/TodoForm.jsx
@@ -10,62 +10,78 @@ import { useState } from "react";
  * { TodoApp, EditableTodo } -> TodoForm
  */
 
-function TodoForm() {
+function TodoForm({ initialFormData, handleSave }) {
+
+  const [formData, setFormData] = useState(initialFormData);
 
   /** Update form input. */
-  function handleChange(evt) { }
+  function handleChange(evt) {
+    const fieldName = evt.target.name;
+    const value = evt.target.value;
+
+    setFormData(currData => {
+      currData[fieldData] = value;
+      return { ...currData };
+    });
+  }
 
   /** Call parent function and clear form. */
-  function handleSubmit(evt) { }
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    handleSave(formData);
+
+    // TODO: determine how to clear form after handleSave is called
+    setFormData();
+  }
 
   return (
-      <form className="TodoForm" onSubmit={handleSubmit}>
+    <form className="TodoForm" onSubmit={handleSubmit}>
 
-        <div className="mb-3">
-          <input
-              name="title"
-              className="form-control"
-              placeholder="Title"
-              onChange={handleChange}
-              value="FIXME"
-              aria-label="Title"
-          />
+      <div className="mb-3">
+        <input
+          name="title"
+          className="form-control"
+          placeholder="Title"
+          onChange={handleChange}
+          value="FIXME"
+          aria-label="Title"
+        />
+      </div>
+
+      <div className="mb-3">
+        <textarea
+          name="description"
+          className="form-control TodoForm-description"
+          placeholder="Description"
+          onChange={handleChange}
+          value="FIXME"
+          aria-label="Description"
+        />
+      </div>
+
+      <div className="mb-3">
+        <div className="w-75 d-flex justify-content-between">
+          <label htmlFor="TodoForm-priority"
+            className="d-inline-flex">Priority:&nbsp;&nbsp;
+          </label>
+          <select id="TodoForm-priority"
+            name="priority"
+            value="FIXME"
+            onChange={handleChange}
+            className=
+            "form-control form-control-sm d-inline-flex TodoForm-priority"
+          >
+            <option value={1}>Ultra-Über</option>
+            <option value={2}>Über</option>
+            <option value={3}>Meh</option>
+          </select>
         </div>
+        <button className="btn-primary btn btn-sm TodoForm-addBtn">
+          Gø!
+        </button>
+      </div>
 
-        <div className="mb-3">
-          <textarea
-              name="description"
-              className="form-control TodoForm-description"
-              placeholder="Description"
-              onChange={handleChange}
-              value="FIXME"
-              aria-label="Description"
-          />
-        </div>
-
-        <div className="mb-3">
-          <div className="w-75 d-flex justify-content-between">
-            <label htmlFor="TodoForm-priority"
-                   className="d-inline-flex">Priority:&nbsp;&nbsp;
-            </label>
-            <select id="TodoForm-priority"
-                    name="priority"
-                    value="FIXME"
-                    onChange={handleChange}
-                    className=
-                      "form-control form-control-sm d-inline-flex TodoForm-priority"
-            >
-              <option value={1}>Ultra-Über</option>
-              <option value={2}>Über</option>
-              <option value={3}>Meh</option>
-            </select>
-          </div>
-          <button className="btn-primary btn btn-sm TodoForm-addBtn">
-            Gø!
-          </button>
-        </div>
-
-      </form>
+    </form>
   );
 }
 
